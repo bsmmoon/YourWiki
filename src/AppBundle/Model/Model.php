f31be6561072c7023ac41dc81c1c92ff11f34b90
seokmin <bsmmoon@gmail.com>
2016-01-23 19:29:58 +0800

    passed the first test

--- src/AppBundle/Model/Model.php
+++ src/AppBundle/Model/Model.php
@@ -6,16 +6,26 @@ class Model
 {
     public function __construct()
     {
-
+        $this->data = [];
     }
 
-    public function run()
+    public function run($input)
     {
-
+        switch($input["command"]) {
+            case "add":
+                $this->addCommand($input["parameters"]);
+                break;
+            default:
+         }
     }
 
     public function getData()
     {
-        
+        return $this->data;
+    }
+
+    private function addCommand($parameters)
+    {
+        $this->data[] = $parameters;
     }
 }
